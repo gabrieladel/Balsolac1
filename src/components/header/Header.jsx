95e222f29638459801a3ae7b807ec9f6d0377a26
gabrieladel <gabi23@live.com.ar>
2024-09-23 20:54:40 -0300

    icono del navbar toggler en blanco

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -16,7 +16,9 @@ function Header() {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span className="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon" style={{
+        backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 30 30'%3E%3Cpath stroke='rgba(255, 255, 255, 1)' stroke-linecap='round' stroke-miterlimit='10' stroke-width='2' d='M4 7h22M4 15h22M4 23h22'/%3E%3C/svg%3E")`
+      }}></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mb-lg-0 bg-dark text-white">
